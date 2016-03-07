7e2132626d4f1bd5dba6b6c0688142718e5adb3a
Ahmed Abudaqqa <ahmed@abudaqqa.com>
2016-03-07 20:07:22 +0400

    add test coverage for the create_docx_report method

---
+++ spec/docx_report/docx_report_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe DocxReport do
+  it 'creates new docx report' do
+    expect(DocxReport.create_docx_report 'spec/files/template.docx')
+      .to be_a DocxReport::Report
+  end
+end
